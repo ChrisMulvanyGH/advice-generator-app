f47d6e37dda15a1a7130573d279ec1b9cc5b7f23
mitlIE <86919933+mitlIE@users.noreply.github.com>
2022-04-16 21:03:02 +0100

    Fetch Quote Advice added

---
+++ app.js
@@ -0,0 +1,25 @@
+(function() {
+    "use strict";
+    
+    const adviceId = document.getElementById('adviceId');
+    const cardQuote = document.getElementById('cardQuote');
+    const diceBtn = document.getElementById('diceBtn');
+
+    diceBtn.addEventListener('click', () => {
+        getAdvice();
+    });
+
+    function getAdvice() {
+        fetch('https://api.adviceslip.com/advice').then(response => {
+            return response.json();
+        }).then(adviceData => {
+            const adviceObject = adviceData.slip;
+            cardQuote.innerHTML = `${adviceObject.advice}`;
+            adviceId.innerHTML = `${adviceObject.id}`
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+
+}());
