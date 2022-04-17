a41f12f890f22f44100facc363c2fe54ad61c5fb
mitlIE <86919933+mitlIE@users.noreply.github.com>
2022-04-17 19:46:49 +0100

    Added a default function to get quote 117 by id

--- app.js
+++ app.js
@@ -5,9 +5,24 @@
     const cardQuote = document.getElementById('cardQuote');
     const diceBtn = document.getElementById('diceBtn');
 
+    defaultQuote();
+    
+    
     diceBtn.addEventListener('click', () => {
         getAdvice();
     });
+
+    function defaultQuote() {
+        fetch('https://api.adviceslip.com/advice/117').then(response => {
+            return response.json();    
+        }).then(adviceData => {
+            const adviceObject = adviceData.slip;
+            cardQuote.innerHTML = `${adviceObject.advice}`;
+            adviceId.innerHTML = `${adviceObject.id}`
+        }).catch(error => {
+            console.log(error);
+        });
+    }
 
     function getAdvice() {
         fetch('https://api.adviceslip.com/advice').then(response => {
